211aeda0968745aeb602a65cc7dea564b5cea463
Ignat Insarov <kindaro@gmail.com>
2015-07-09 20:32:20 +0300

    Removed build -- it clutters.

--- build/js/script.js
+++
@@ -1,9 +0,0 @@
-var app;
-
-app = angular.module('app', []);
-
-app.controller('ctrl', function($scope, $http) {
-  return $http.get("/content/content.json").success(function(response) {
-    return $scope.content = response;
-  });
-});
